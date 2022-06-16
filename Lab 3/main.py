73385e280069802c6c4cb6745a5502461f0e47dd
Mihir Bhanderi <82660793+Mihir1310@users.noreply.github.com>
2022-06-16 11:31:43 +0530

    SLR

--- Lab 3/main.py
+++ Lab 3/main.py
@@ -1,99 +1,82 @@
-# Multiple Linear Regression Gradient Descent
+#Simple Linear Regression
 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import metrics, datasets
-from sklearn.preprocessing import StandardScaler
-
+from sklearn import metrics
 
 def Plot_CF(theta1_Val, Cf):
-    plt.plot(theta1_Val, Cf)
+    plt.plot(theta1_Val, Cf, color='red')
+    plt.show()
+
+
+def Plot_TrainSet(x_train, y_train, predytrain):
+    plt.scatter(x_train[:, 1], y_train)
+    plt.plot(x_train[:, 1], predytrain, color='red')
+    plt.show()
+
+
+def Plot_TestSet(x_test, y_test, predytest):
+    plt.scatter(x_test[:, 1], y_test)
+    plt.plot(x_test[:, 1], predytest, color='red')
     plt.show()
 
 
 if __name__ == '__main__':
-    X, y = datasets.load_boston(return_X_y=True)
+    df1 = pd.read_csv("Train.csv")
+    df2 = pd.read_csv("Test.csv")
+    x_train = np.ones((699, 2))
+    x_train[:, 1] = df1['x']
+    y_train = np.array(df1['y'])
 
-    X_train_temp1 = X[0:400, :]
-    X_train = np.ones((X_train_temp1.shape[0], X_train_temp1.shape[1] + 1))
-    X_train[:, 1:] = X_train_temp1
-    y_train = y[:400]
-    X_test_temp1 = X[400:506, :]
-    X_test = np.ones((X_test_temp1.shape[0], X_test_temp1.shape[1] + 1))
-    X_test[:, 1:] = X_test_temp1
-    y_test = y[400:506]
+    x_test = np.ones((300, 2))
+    x_test[:, 1] = df2['x']
+    y_test = np.array(df2['y'])
 
-    m = X_train.shape[0]
-    n = X_train.shape[1]
+    m = x_train.shape[0]
+    n = x_train.shape[1]
 
-    scaler = StandardScaler()
-    scaler.fit(X_train[:,1:])
-    X_train[:,1:] = scaler.transform(X_train[:,1:])
-    X_test[:,1:] = scaler.transform(X_test[:, 1:])
-
-    theta = np.random.uniform(0,1,n)
-    alpha = 0.01
-    nitterations = 1000
+    theta = (-3, 2)
+    alpha = 0.0001
+    nitterations = 100
 
     theta1_Val = []
     Cf = list()
 
     for i in range(nitterations):
-        update = np.zeros(X_train.shape[1])
-        ypred = np.dot(X_train, theta)
+        update = np.zeros(x_train.shape[1])
+        ypred = np.dot(x_train, theta)
         error = ypred - y_train
         for j in range(n):
-            update[j] = np.sum(np.dot(error, X_train[:, j]))
+            update[j] = np.sum(np.dot(error, x_train[:, j]))
 
         theta = theta - (1 / m) * alpha * update
-        theta1_Val.append(theta[0])
+        theta1_Val.append(theta[1])
 
         # Cost-Function
         sum1 = 0
         for k in range(m):
-            sum2 = 0
-            for i in range(n):
-                sum2 += theta[i] * X_train[k, i]
-            sum1 += (sum2 - y_train[k]) ** 2
+            sum1 += (theta[0] + theta[1] * x_train[k, 1] - y_train[k]) ** 2
 
         Cf.append(sum1 * (1 / (2 * m)))
 
-    print("Theta : ", theta)
-    print(("Theta[1] : ", len(theta1_Val)))
-
-    print("CF : ", len(Cf))
-    prediction = np.dot(theta, X_test.T)
+    prediction = np.dot(theta, x_test.T)
     print("MAE: ", metrics.mean_absolute_error(y_true=y_test, y_pred=prediction))
     print("MSE: ", metrics.mean_squared_error(y_true=y_test, y_pred=prediction))
 
+    predytrain = theta[0] + theta[1] * x_train[:, 1]
+    predytest = theta[0] + theta[1] * x_test[:, 1]
 
     print("\nPlotting of Graphs : ")
-    print("""    1. Cost Function
-    2. EXIT""")
+    print("""        1. Cost Function
+        2. Training Set Model
+        3. Test Set Model
+        4. EXIT""")
     while True:
         n = int(input("Enter What to Plot : "))
         if n == 1:
             Plot_CF(theta1_Val, Cf)
-        else:
-            exit("Thanks for Using")
-
-"""
-OUTPUT:
-
-Theta :  [ 2.43334807e+01 -1.00515791e+00  9.15022551e-01  1.13357521e-02
-  5.61123609e-01 -1.20836710e+00  3.73442039e+00 -2.73681723e-02
- -2.54183088e+00  2.06095317e+00 -1.08449809e+00 -1.65437094e+00
-  5.40425479e-02 -3.47470775e+00]
-('Theta[1] : ', 1000)
-CF :  1000
-MAE:  4.771415037813911
-MSE:  32.86957950538916
-
-Plotting of Graphs : 
-    1. Cost Function
-    2. EXIT
-Enter What to Plot : 1
-Enter What to Plot : 2
-Thanks for Using
-
-"""
+        if n == 2:
+            Plot_TrainSet(x_train, y_train, predytrain)
+        elif n == 3:
+            Plot_TestSet(x_test, y_test, predytest)
